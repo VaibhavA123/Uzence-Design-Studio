7812810fcfb0940f694f9043f83e825576a1adda
VaibhavA123 <160752272+VaibhavA123@users.noreply.github.com>
2025-08-30 14:42:08 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // adjust if your files are elsewhere
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
